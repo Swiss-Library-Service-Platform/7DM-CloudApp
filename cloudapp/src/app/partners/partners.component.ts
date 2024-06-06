f33b6053dcd9e24c783fd5e11e135369d1ed75f4
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2024-06-06 17:02:39 +0200

    Add request tab

--- cloudapp/src/app/partners/partners.component.ts
+++ cloudapp/src/app/partners/partners.component.ts
@@ -3,55 +3,55 @@ import { Component, OnInit } from '@angular/core';
 import { CloudAppEventsService } from '@exlibris/exl-cloudapp-angular-lib';
 
 @Component({
-  selector: 'app-partners',
-  templateUrl: './partners.component.html',
-  styleUrls: ['./partners.component.scss']
+    selector: 'app-partners',
+    templateUrl: './partners.component.html',
+    styleUrls: ['./partners.component.scss']
 })
 export class PartnersComponent implements OnInit {
-  originalResponse: any;
-  response: any;
-  isLoading = false;
-  searchValue = '';
-  authToken: string;
+    originalResponse: any;
+    response: any;
+    isLoading = false;
+    searchValue = '';
+    authToken: string;
 
-  constructor(private http: HttpClient, private eventsService: CloudAppEventsService) { }
+    constructor(private http: HttpClient, private eventsService: CloudAppEventsService) { }
 
-  ngOnInit() {
-    this.eventsService.getAuthToken().subscribe(authToken => {
-      this.authToken = authToken
-      this.loadPartners();
-    });
-  }
-
-  loadPartners() {
-    if (this.originalResponse) {
-      console.log("Partners already loaded");
-      return;
+    ngOnInit() {
+        this.eventsService.getAuthToken().subscribe(authToken => {
+            this.authToken = authToken
+            this.loadPartners();
+        });
     }
 
-    this.isLoading = true;
-    const headers = { 'Authorization': `Bearer ${this.authToken}` };
-    console.log("Headers: " + JSON.stringify(headers));
-    this.http.get('http://localhost:4200/api/v1/partners', { headers }).subscribe(
-      response => {
-        this.originalResponse = response;
-        this.response = [...this.originalResponse];
-        this.isLoading = false;
-        console.log("Partners retrieved successfully!, at: " + new Date().toLocaleTimeString() + " on " + new Date().toLocaleDateString());
-      },
-      error => {
-        console.error('Error: ' + error);
-        console.log(JSON.stringify(error))
-        this.isLoading = false;
-      }
-    );
-  }
+    loadPartners() {
+        if (this.originalResponse) {
+            console.log("Partners already loaded");
+            return;
+        }
 
-  search() {
-    if (!this.searchValue) {
-      this.response = [...this.originalResponse];
-    } else {
-      this.response = this.originalResponse.filter((item: any) => item.name.toLowerCase().includes(this.searchValue.toLowerCase()));
+        this.isLoading = true;
+        const headers = { 'Authorization': `Bearer ${this.authToken}` };
+        console.log("Headers: " + JSON.stringify(headers));
+        this.http.get('http://localhost:4200/api/v1/partners', { headers }).subscribe(
+            response => {
+                this.originalResponse = response;
+                this.response = [...this.originalResponse];
+                this.isLoading = false;
+                console.log("Partners retrieved successfully!, at: " + new Date().toLocaleTimeString() + " on " + new Date().toLocaleDateString());
+            },
+            error => {
+                console.error('Error: ' + error);
+                console.log(JSON.stringify(error))
+                this.isLoading = false;
+            }
+        );
     }
-  }
+
+    search() {
+        if (!this.searchValue) {
+            this.response = [...this.originalResponse];
+        } else {
+            this.response = this.originalResponse.filter((item: any) => item.name.toLowerCase().includes(this.searchValue.toLowerCase()));
+        }
+    }
 }
--- cloudapp/src/app/requests/requests.component.ts
+++ cloudapp/src/app/requests/requests.component.ts
@@ -3,52 +3,62 @@ import { CloudAppEventsService, InitData } from '@exlibris/exl-cloudapp-angular-
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-requests',
-  templateUrl: './requests.component.html',
-  styleUrls: ['./requests.component.scss']
+    selector: 'app-requests',
+    templateUrl: './requests.component.html',
+    styleUrls: ['./requests.component.scss']
 })
 export class RequestsComponent implements OnInit {
-  data: InitData;
-  requestID: string = '';
-  authToken: string;
+    data: InitData;
+    requestID: string = '';
+    authToken: string;
+    isLoading = false;
+    failed = false;
+    response: any;
 
-  constructor(private http: HttpClient, private eventsService: CloudAppEventsService) { }
+    constructor(private http: HttpClient, private eventsService: CloudAppEventsService) { }
 
-  ngOnInit(): void {
-    this.eventsService.getAuthToken().subscribe(authToken => {
-      this.authToken = authToken;
-      console.log("Auth set");
-    });
+    ngOnInit(): void {
+        this.eventsService.getAuthToken().subscribe(authToken => {
+            this.authToken = authToken;
+            console.log("Auth set");
+        });
 
-    this.eventsService.getInitData().subscribe(
-      data => {
-        this.data = data;
-        console.log(data.user.currentlyAtLibCode)
-      }
-    );
-  }
-
-  lookUpRequest(): void {
-    let libraryCode = this.data.user.currentlyAtLibCode;
-    if (!this.requestID) {
-      console.error("No input value");
-      return;
+        this.eventsService.getInitData().subscribe(
+            data => {
+                this.data = data;
+                console.log(data.user.currentlyAtLibCode)
+            }
+        );
     }
 
-    console.log("Retrieving request with ID: " + this.requestID + " from library: " + libraryCode);
-    // /api/v1/requests/{requestId}/libraries/{libraryId}
+    lookUpRequest(): void {
+        let libraryCode = this.data.user.currentlyAtLibCode;
+        if (!this.requestID) {
+            console.error("No input value");
+            return;
+        }
 
-    const headers = { 'Authorization': `Bearer ${this.authToken}` };
-    console.log("Headers: " + JSON.stringify(headers));
-    this.http.get(`http://localhost:4200/api/v1/requests/${this.requestID}/libraries/${libraryCode}`, { headers }).subscribe(
-      response => {
-        console.log(JSON.stringify(response));
-        console.log("Partners retrieved successfully!, at: " + new Date().toLocaleTimeString() + " on " + new Date().toLocaleDateString());
-      },
-      error => {
-        console.error('Error: ' + error);
-        console.log(JSON.stringify(error));
-      }
-    );
-  }
+        this.isLoading = true;
+        this.failed = false;
+        this.response = null;
+        console.log("Retrieving request with ID: " + this.requestID + " from library: " + libraryCode);
+        // /api/v1/requests/{requestId}/libraries/{libraryId}
+
+        const headers = { 'Authorization': `Bearer ${this.authToken}` };
+        console.log("Headers: " + JSON.stringify(headers));
+        this.http.get(`http://localhost:4200/api/v1/requests/${this.requestID}/libraries/${libraryCode}`, { headers }).subscribe(
+            response => {
+                this.response = response;
+                console.log(JSON.stringify(response));
+                console.log("Partners retrieved successfully!, at: " + new Date().toLocaleTimeString() + " on " + new Date().toLocaleDateString());
+                this.isLoading = false;
+            },
+            error => {
+                console.error('Error: ' + error);
+                console.log(JSON.stringify(error));
+                this.failed = true;
+                this.isLoading = false;
+            }
+        );
+    }
 }
