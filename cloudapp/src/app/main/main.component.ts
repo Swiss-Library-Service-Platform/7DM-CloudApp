b2717d9cc72c00e0952f806af050bfc944ac4943
Sebastian Voigt <35730788+VoigtSebastian@users.noreply.github.com>
2024-06-10 14:00:59 +0200

    Add is-allowed check

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -1,12 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CloudAppEventsService } from '@exlibris/exl-cloudapp-angular-lib';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss']
 })
 
 export class MainComponent implements OnInit {
-  constructor() { }
-  ngOnInit(): void { }
+    loading = true;
+    authToken: string;
+    allowed = false;
+
+    constructor(private http: HttpClient, private eventsService: CloudAppEventsService) { }
+
+    ngOnInit(): void {
+        this.eventsService.getAuthToken().subscribe(authToken => {
+            this.authToken = authToken
+            this.checkAllowed();
+        });
+    }
+
+    checkAllowed(): void {
+        if (!this.loading) {
+            return;
+        }
+
+        const headers = { 'Authorization': `Bearer ${this.authToken}` };
+        this.http.get('http://localhost:4200/api/v1/allowed', { headers }).subscribe(
+            _ => {
+                this.loading = false;
+                this.allowed = true;
+            },
+            error => {
+                console.log(JSON.stringify(error))
+                this.loading = false;
+                this.allowed = false;
+            }
+        );
+    }
 }
