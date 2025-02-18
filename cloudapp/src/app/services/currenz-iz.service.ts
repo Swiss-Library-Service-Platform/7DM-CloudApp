0d9e895e8a16e601b331abe789030e54dd8ed1b4
v1lling <sascha.villing@web.de>
2025-02-18 09:56:59 +0100

    Implement box label browser caching for UZB

--- cloudapp/src/app/services/currenz-iz.service.ts
+++ cloudapp/src/app/services/currenz-iz.service.ts
@@ -7,16 +7,22 @@ import { CloudAppEventsService, InitData } from '@exlibris/exl-cloudapp-angular-
 })
 export class CurrentIzService {
   private _isCurrentIzNetworkZone = new BehaviorSubject<boolean>(false);
+  private _isCurrentIzUZB = new BehaviorSubject<boolean>(false);
 
   constructor(private eventsService: CloudAppEventsService) {
     this.eventsService.getInitData().subscribe(
       initData => {
         this._isCurrentIzNetworkZone.next(initData.instCode == '41SLSP_NETWORK');
+        this._isCurrentIzUZB.next(initData.instCode == '41SLSP_UZB');
       }
     );
   }
 
   get isCurrentIzNetworkZone(): boolean {
     return this._isCurrentIzNetworkZone.value;
+  }
+
+  get isCurrentIzUZB(): boolean {
+    return this._isCurrentIzUZB.value;
   }
 }
